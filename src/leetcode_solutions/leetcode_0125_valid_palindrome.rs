d1cdc145c9d0c57ef49e8f06904be7b5bda0938b
Stephen King <kingze1992@163.com>
2022-11-10 14:43:20 +0800

    add leetcode 0125 Valid Palindrome

---
+++ src/leetcode_solutions/leetcode_0125_valid_palindrome.rs
@@ -0,0 +1,33 @@
+struct Solution;
+
+impl Solution {
+    pub fn is_palindrome(s: String) -> bool {
+        let s: Vec<char> = s
+            .chars()
+            .filter(|c| c.is_alphanumeric())
+            .map(|c| c.to_ascii_lowercase())
+            .collect();
+
+        let a: String = s.iter().collect();
+        let b: String = s.iter().rev().collect();
+
+        a == b
+    }
+}
+
+#[cfg(test)]
+mod leetcode_tests {
+    use super::Solution;
+
+    #[test]
+    fn leetcode_0125_test() {
+        assert!(Solution::is_palindrome(String::from("121")));
+        assert!(Solution::is_palindrome(String::from("12321")));
+        assert!(!Solution::is_palindrome(String::from("1234")));
+        assert!(Solution::is_palindrome(String::from(
+            "A man, a plan, a canal: Panama"
+        )));
+        assert!(!Solution::is_palindrome(String::from("race a car")));
+        assert!(Solution::is_palindrome(String::from(" ")));
+    }
+}
