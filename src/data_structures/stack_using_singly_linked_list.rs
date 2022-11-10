3c9ef93dd6bbc4106a8a0c241ad5568d2074aa6a
Stephen King <kingze1992@163.com>
2022-11-10 17:33:43 +0800

    add iter functions

--- src/data_structures/stack_using_singly_linked_list.rs
+++ src/data_structures/stack_using_singly_linked_list.rs
@@ -33,10 +33,9 @@ impl<T> Stack<T> {
             Some(node) => {
                 self.top = node.next;
                 Ok(node.elem)
-            },
+            }
             None => Err("Stack is empty"),
         }
-
     }
 
     pub fn peek(&self) -> Option<&T> {
@@ -57,16 +56,20 @@ impl<T> Stack<T> {
         self.top.is_none()
     }
 
-    pub fn iter() {
-        // todo
+    pub fn into_iter_for_stack(self) -> IntoIter<T> {
+        IntoIter(self)
     }
 
-    pub fn iter_mut() {
-        // todo
+    pub fn iter(&self) -> Iter<T> {
+        Iter {
+            next: self.top.as_deref(),
+        }
     }
 
-    pub fn into_iter_for_stack() {
-        // todo
+    pub fn iter_mut(&mut self) -> IterMut<T> {
+        IterMut {
+            next: self.top.as_deref_mut(),
+        }
     }
 }
 
@@ -78,10 +81,50 @@ impl<T> Default for Stack<T> {
 
 impl<T> Drop for Stack<T> {
     fn drop(&mut self) {
-        // todo: implement drop function using iterator
-        while !self.is_empty() {
-            self.pop().unwrap();
+        let mut link = self.top.take();
+        while let Some(mut node) = link {
+            link = node.next.take();
         }
+    }
+}
+
+pub struct IntoIter<T>(Stack<T>);
+
+impl<T> Iterator for IntoIter<T> {
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.pop().ok()
+    }
+}
+
+pub struct Iter<'a, T> {
+    next: Option<&'a Node<T>>,
+}
+
+impl<'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.map(|node| {
+            self.next = node.next.as_deref();
+            &node.elem
+        })
+    }
+}
+
+pub struct IterMut<'a, T> {
+    next: Option<&'a mut Node<T>>,
+}
+
+impl<'a, T> Iterator for IterMut<'a, T> {
+    type Item = &'a mut T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.take().map(|node| {
+            self.next = node.next.as_deref_mut();
+            &mut node.elem
+        })
     }
 }
 
@@ -90,17 +133,91 @@ mod stack_tests {
     use super::Stack;
 
     #[test]
-    fn basics_test() {
-        let mut stack: Stack<i32> = Stack::new();
-        assert_eq!(stack.pop(), Err("Stack is empty"));
-        for i in 0..10 {
-            stack.push(i);
-        }
-        println!("{:?}", stack);
+    fn basics() {
+        let mut list = Stack::new();
+        assert_eq!(list.pop(), Err("Stack is empty"));
 
-        while !stack.is_empty() {
-            stack.pop().unwrap();
-        }
-        println!("{:?}", stack);
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        assert_eq!(list.pop(), Ok(3));
+        assert_eq!(list.pop(), Ok(2));
+
+        list.push(4);
+        list.push(5);
+
+        assert!(!list.is_empty());
+
+        assert_eq!(list.pop(), Ok(5));
+        assert_eq!(list.pop(), Ok(4));
+
+        assert_eq!(list.pop(), Ok(1));
+        assert_eq!(list.pop(), Err("Stack is empty"));
+
+        assert!(list.is_empty());
+    }
+
+    #[test]
+    fn peek() {
+        let mut list = Stack::new();
+        assert_eq!(list.peek(), None);
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        assert_eq!(list.peek(), Some(&3));
+        assert_eq!(list.peek_mut(), Some(&mut 3));
+
+        match list.peek_mut() {
+            None => None,
+            Some(value) => {
+                *value = 42;
+                Some(())
+            }
+        };
+
+        assert_eq!(list.peek(), Some(&42));
+        assert_eq!(list.pop(), Ok(42));
+    }
+
+    #[test]
+    fn into_iter() {
+        let mut list = Stack::new();
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        let mut iter = list.into_iter_for_stack();
+        assert_eq!(iter.next(), Some(3));
+        assert_eq!(iter.next(), Some(2));
+        assert_eq!(iter.next(), Some(1));
+        assert_eq!(iter.next(), None);
+    }
+
+    #[test]
+    fn iter() {
+        let mut list = Stack::new();
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        let mut iter = list.iter();
+        assert_eq!(iter.next(), Some(&3));
+        assert_eq!(iter.next(), Some(&2));
+        assert_eq!(iter.next(), Some(&1));
+    }
+
+    #[test]
+    fn iter_mut() {
+        let mut list = Stack::new();
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        let mut iter = list.iter_mut();
+        assert_eq!(iter.next(), Some(&mut 3));
+        assert_eq!(iter.next(), Some(&mut 2));
+        assert_eq!(iter.next(), Some(&mut 1));
     }
 }
