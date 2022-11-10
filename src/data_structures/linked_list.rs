160b7caa48eab15a537a8d39e0464bc0589d3a9f
Stephen King <kingze1992@163.com>
2022-11-10 17:16:52 +0800

    add data structure linked_list

---
+++ src/data_structures/linked_list.rs
@@ -0,0 +1,159 @@
+/// singly linked list
+pub struct LinkedList<T> {
+    size: usize,
+    head: Link<T>,
+}
+
+type Link<T> = Option<Box<Node<T>>>;
+
+struct Node<T> {
+    elem: T,
+    next: Link<T>,
+}
+
+impl<T> LinkedList<T> {
+    pub fn new() -> Self {
+        Self {
+            size: 0,
+            head: None,
+        }
+    }
+
+    pub fn push(&mut self, elem: T) {
+        let new_node = Box::new(Node {
+            elem,
+            next: self.head.take(),
+        });
+        self.head = Some(new_node);
+        self.size += 1;
+    }
+
+    pub fn pop(&mut self) -> Option<T> {
+        self.head.take().map(|node| {
+            self.head = node.next;
+            self.size -= 1;
+            node.elem
+        })
+    }
+
+    pub fn peek(&self) -> Option<&T> {
+        self.head.as_ref().map(|node| &node.elem)
+    }
+
+    pub fn peek_mut(&mut self) -> Option<&mut T> {
+        self.head.as_mut().map(|node| &mut node.elem)
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.size == 0
+    }
+
+    pub fn size(&self) -> usize {
+        self.size
+    }
+
+    pub fn into_iter_for_linked_list(self) -> IntoIter<T> {
+        IntoIter(self)
+    }
+
+    pub fn iter(&self) -> Iter<T> {
+        Iter {
+            next: self.head.as_deref(),
+        }
+    }
+
+    pub fn iter_mut(&mut self) -> IterMut<T> {
+        IterMut {
+            next: self.head.as_deref_mut(),
+        }
+    }
+}
+
+impl<T> Default for LinkedList<T> {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl<T> Drop for LinkedList<T> {
+    fn drop(&mut self) {
+        let mut link = self.head.take();
+        while let Some(mut node) = link {
+            link = node.next.take();
+        }
+    }
+}
+
+pub struct IntoIter<T>(LinkedList<T>);
+
+impl<T> Iterator for IntoIter<T> {
+    type Item = T;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.pop()
+    }
+}
+
+pub struct Iter<'a, T> {
+    next: Option<&'a Node<T>>,
+}
+
+impl<'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.map(|node| {
+            self.next = node.next.as_deref();
+            &node.elem
+        })
+    }
+}
+
+pub struct IterMut<'a, T> {
+    next: Option<&'a mut Node<T>>,
+}
+
+impl<'a, T> Iterator for IterMut<'a, T> {
+    type Item = &'a mut T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.take().map(|node| {
+            self.next = node.next.as_deref_mut();
+            &mut node.elem
+        })
+    }
+}
+
+#[cfg(test)]
+mod data_structures_tests {
+    use super::LinkedList;
+
+    #[test]
+    fn test_basics() {
+        let mut list: LinkedList<i32> = LinkedList::new();
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        assert_eq!(list.pop(), Some(3));
+        assert_eq!(list.peek(), Some(&2));
+        assert_eq!(list.peek_mut(), Some(&mut 2));
+        if let Some(value) = list.peek_mut() {
+            *value = 5;
+        }
+        assert_eq!(list.peek(), Some(&5));
+    }
+
+    #[test]
+    fn test_into_iter() {
+        let mut list: LinkedList<i32> = LinkedList::new();
+        list.push(1);
+        list.push(2);
+        list.push(3);
+    }
+
+    #[test]
+    fn test_iter() {}
+
+    #[test]
+    fn test_iter_mut() {}
+}
