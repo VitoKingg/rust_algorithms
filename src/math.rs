d4c468d2fb24867eff5337d4f69b8e3940212936
Stephen King <kingze1992@163.com>
2022-11-10 11:09:28 +0800

    add math base_conversion

--- src/math.rs
+++ src/math.rs
@@ -1,5 +1,7 @@
-mod lcm_of_n_numbers;
+mod base_conversion;
 mod gcd_of_n_numbers;
+mod lcm_of_n_numbers;
 
-pub use self::lcm_of_n_numbers::lcm_of_n_numbers;
+pub use self::base_conversion::base_conversion;
 pub use self::gcd_of_n_numbers::gcd_of_n_numbers;
+pub use self::lcm_of_n_numbers::lcm_of_n_numbers;
---
+++ src/math/base_conversion.rs
@@ -0,0 +1,31 @@
+pub fn base_conversion(mut deci_num: u32, base_num: u32) -> String {
+    let digits = [
+        '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F',
+    ];
+
+    let mut remainder_stack = vec![];
+    while deci_num > 0 {
+        let remainder = deci_num % base_num;
+        remainder_stack.push(remainder);
+        deci_num /= base_num;
+    }
+
+    let mut base_str = String::from("");
+    while let Some(remainder) = remainder_stack.pop() {
+        base_str += &digits[remainder as usize].to_string();
+    }
+
+    base_str
+}
+
+#[cfg(test)]
+mod math_tests {
+    use super::base_conversion;
+
+    #[test]
+    fn base_conversion_test() {
+        assert_eq!(base_conversion(10, 2), String::from("1010"));
+        assert_eq!(base_conversion(43, 16), String::from("2B"));
+        assert_eq!(base_conversion(121, 8), String::from("171"));
+    }
+}
