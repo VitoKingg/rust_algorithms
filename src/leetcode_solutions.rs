2d1b66aab504d62f825dd1b1456613e46fa12181
Stephen King <kingze1992@163.com>
2022-11-10 14:42:34 +0800

    allow dead_code

--- src/leetcode_solutions.rs
+++ src/leetcode_solutions.rs
@@ -1,2 +1,7 @@
-mod leetcode_0125_valid_palindrome;
+#![allow(dead_code)]
+
 mod leetcode_0020_valid_parentheses;
+mod leetcode_0125_valid_palindrome;
+
+pub use self::leetcode_0020_valid_parentheses::*;
+pub use self::leetcode_0125_valid_palindrome::*;
